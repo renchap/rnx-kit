e2fc284f6ce552ca345deb0ae4fa18c62e6ef1f6
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2023-03-08 13:05:31 +0100

    "[rnx-build] changes not staged for commit"

--- incubator/build/src/build.ts
+++ incubator/build/src/build.ts
@@ -9,56 +9,78 @@ import type {
   RepositoryInfo,
 } from "./types.js";
 
+function createReporter() {
+  return new TaskReporter({
+    productName: "@rnx-kit/build",
+    description: `Building`,
+    showStarted: false,
+    showPending: true,
+    showCompleted: true,
+    showSummary: false,
+    showTaskDetails: false,
+  });
+}
+
 export async function startBuild(
   remote: Remote,
   distribution: DistributionPlugin,
   repoInfo: RepositoryInfo,
   inputs: BuildParams
 ): Promise<number> {
-  const spinner = ora();
-
-  spinner.start("Creating build branch");
-
   const upstream = "origin";
-  const buildBranch = await pushCurrentChanges(upstream);
-  if (!buildBranch) {
-    return 1;
-  }
+  const reporter = createReporter();
 
-  spinner.succeed(`Created build branch ${buildBranch}`);
-
-  const context = { ...repoInfo, ref: buildBranch };
-  const cleanUp = once(async () => {
-    spinner.start("Cleaning up");
-    await Promise.allSettled([
-      remote.cancelBuild(context),
-      deleteBranch(buildBranch, upstream),
-    ]);
-    spinner.succeed(`Deleted ${buildBranch}`);
+  let buildBranch: string;
+  await reporter.runTask("Creating build branch", async () => {
+    buildBranch = await pushCurrentChanges(upstream) || "";
+    return {
+      status: buildBranch ? "complete" : "fail",
+      message: buildBranch || "Failed to create build branch",
+    };
   });
+
+  // @ts-expect-error asdf
+  const context = { ...repoInfo, ref: buildBranch };
+  const cleanUp = once(() =>
+    reporter.runTask("Cleaning up", async () => {
+      await Promise.allSettled([
+        remote.cancelBuild(context),
+        deleteBranch(buildBranch, upstream),
+      ]);
+      return {
+        status: "complete",
+        message: `Deleted ${buildBranch}`,
+      };
+    })
+  );
+
   const onSignal = () => {
-    spinner.fail();
+    reporter.complete("Cancelled");
     cleanUp().then(() => process.exit(1));
   };
+
   process.on("SIGINT", onSignal);
   process.on("SIGTERM", onSignal);
 
-  spinner.start("Queueing build");
+  if (!reporter) {
+    const spinner = ora();
+    try {
+      const artifact = await remote.build(context, inputs, spinner);
+      if (!artifact) {
+        await cleanUp();
+        return 1;
+      }
 
-  try {
-    const artifact = await remote.build(context, inputs, spinner);
-    if (!artifact) {
+      await distribution.deploy({ ...context, ...inputs }, artifact, spinner);
+    } catch (e) {
+      spinner.fail();
       await cleanUp();
-      return 1;
+      throw e;
     }
 
-    await distribution.deploy({ ...context, ...inputs }, artifact, spinner);
-  } catch (e) {
-    spinner.fail();
     await cleanUp();
-    throw e;
+    return 0;
   }
 
-  await cleanUp();
-  return 0;
+  return 1;
 }
